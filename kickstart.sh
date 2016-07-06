73b4bb407dd6e12c1e5e6bac5886c27782d98c44
Mr. Is <isaac.ardis@gmail.com>
2016-07-06 10:19:41 -0400

    kickstart for freya

---
+++ kickstart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# run from project dir
+
+echo "killing running chat process..."
+kill $(ps aux | grep chat | awk '{print $2}')
+
+echo "rebuilding chat from go"
+go build -o chat chat.go
+
+echo "starting chat with out to chat.log"
+./chat &>"$PWD/chat.log" &!
+
