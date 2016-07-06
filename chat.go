36d7de2a6c92e5dd2885b2a8e92fe2ad0aa1e535
jlanej <jlanej@gmail.com>
2016-07-05 22:26:22 -0500

    High performance chat database

--- chat.go
+++ chat.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/olahol/melody"
-	"net/http"
 )
+
+// f, err := os.OpenFile(filename, os.O_APPEND, 0666)
+
+// n, err := f.WriteString(text)
+
+// f.Close()
 
 func main() {
 	r := gin.Default()
 	m := melody.New()
+
+	r.StaticFile("/test.txt", "./test.txt")
 
 	r.GET("/", func(c *gin.Context) {
 		http.ServeFile(c.Writer, c.Request, "index.html")
@@ -20,6 +32,17 @@ func main() {
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		m.Broadcast(msg)
+		f, err := os.OpenFile("./test.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		if err != nil {
+			log.Fatalln("Error opening file: ", err)
+		}
+		bytes, err := f.WriteString(string(msg) + "\n")
+		if err != nil {
+			log.Fatalln("Error writing string: ", err)
+		}
+		fmt.Printf("Wrote %d bytes to file\n", bytes)
+		fmt.Println(string(msg))
+		f.Close()
 	})
 
 	r.Run(":5000")
