ab41eb30feca88281ebf93623ab5a79b89d7e2b0
Mr. Is <isaac.ardis@gmail.com>
2016-07-06 10:24:53 -0400

    bourne to kill

--- kickstart.sh
+++ kickstart.sh
@@ -2,6 +2,10 @@
 
 # run from project dir
 
+echo "killing running chat process... " 
+echo "(will fail to kill ps proc)"
+kill $(ps aux | grep chat | awk '{print $2}')
+
 echo "rebuilding chat from go"
 go build -o chat chat.go
 
