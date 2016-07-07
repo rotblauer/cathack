40232c539fc8d99a5230643f470716abe09d6677
jlanej <jlanej@gmail.com>
2016-07-06 22:59:46 -0500

    sms placeholder

--- chat.go
+++ chat.go
@@ -5,17 +5,38 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/njern/gonexmo"
 	"github.com/olahol/melody"
 	j "github.com/ricardolonga/jsongo"
 
 	"./lib"
 )
 
-// func formatPS1()
+//SMS text sender, nexmo to test...need a sign up with keys
+func sms(number string, messageToSend string, key string, secret string) {
+	nexmoClient, _ := nexmo.NewClientFromAPI(key, secret)
+	// https://github.com/njern/gonexmo
+	// Send an SMS
+	// See https://docs.nexmo.com/index.php/sms-api/send-message for details.
+	message := &nexmo.SMSMessage{
+		From:            "12529178592",
+		To:              number,
+		Type:            nexmo.Text,
+		Text:            messageToSend,
+		ClientReference: "gonexmo-test " + strconv.FormatInt(time.Now().Unix(), 10),
+		Class:           nexmo.Standard,
+	}
+
+	messageResponse, err := nexmoClient.SMS.Send(message)
+	if err != nil {
+		log.Fatalln("Error getting sending sms: ", err)
+	}
+	fmt.Println(messageResponse)
+}
 
 func main() {
 	//go run chat.go
@@ -56,23 +77,24 @@ func main() {
 		if err != nil {
 			log.Fatalln("Error getting Geo IP.", err)
 		}
-		
-		data := j.Object().Put("time", timeString). // btw hanging dots are no go
-											 Put("unixNano", timeUnixNano).
-											 Put("message", string(msg)).
-											 Put("ip", ip).
-											 Put("bootsIP", lib.BootsEncoded(ip)).
-											 Put("lat", geoip["lat"]).
-											 Put("lon", geoip["lon"]).
-											 Put("city", geoip["city"]).
-											 Put("subdiv", geoip["subdiv"]).
-											 Put("countryIsoCode", geoip["countryIsoCode"]).
-											 Put("tz", geoip["tz"])
-										
-		dataIndentedString := data.String()
-		ps1 := []byte(dataIndentedString)
 
-		// Broadcast web socket. 
+		data := j.Object().Put("time", timeString). // btw hanging dots are no go
+								Put("unixNano", timeUnixNano).
+								Put("message", string(msg)).
+								Put("ip", ip).
+								Put("bootsIP", lib.BootsEncoded(ip)).
+								Put("lat", geoip["lat"]).
+								Put("lon", geoip["lon"]).
+								Put("city", geoip["city"]).
+								Put("subdiv", geoip["subdiv"]).
+								Put("countryIsoCode", geoip["countryIsoCode"]).
+								Put("tz", geoip["tz"])
+
+		dataIndentedString := data.String()
+		//sms("12182606849", dataIndentedString, "c330fe3b", "d69e9ca6c8245f6a")
+
+		ps1 := []byte(dataIndentedString)
+		// Broadcast web socket.
 		// @ps1 []byte
 		m.Broadcast(ps1)
 
@@ -97,4 +119,3 @@ func main() {
 
 	r.Run(":5000")
 }
-
